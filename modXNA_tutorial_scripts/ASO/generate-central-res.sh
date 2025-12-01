11946aafc641fec735e1c1f10d1357b780aaddcc
Christina Bergonzo <christina.bergonzo@nist.gov>
2025-12-01 09:08:28 -0500

    Add all files section 3.3 ASO

---
+++ modXNA_tutorial_scripts/ASO/generate-central-res.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+WORKDIR=`pwd`
+
+modxna="/home/bergonzoc/GitHub/modXNA.dan/modxna.sh"
+
+declare -A ResNames
+
+ResNames["5CG"]="5PO CET DGG"
+ResNames["CEG"]="DPO CET DGG"
+ResNames["MEC"]="DPO CET M5C"
+ResNames["MOG"]="DPO MOE RGG"
+ResNames["MOA"]="DPO MOE RAA"
+ResNames["POG"]="PS1 MOE RGG"
+ResNames["POA"]="DPO MOE RAA"
+ResNames["MSC"]="PS1 CET M5C"
+ResNames["OMU"]="PS1 OME RUU"
+ResNames["3EC"]="PS1 CET M5C"
+
+
+#for res in CEG MEC MOG MOA POG POA MSC OMU ; do
+
+#mkdir $res/
+#cd $res/
+
+#echo "${ResNames[$res]}" > $res.modxna.in
+
+#$modxna -i $res.modxna.in -m $res
+
+#cd $WORKDIR
+
+#done
+
+#5' 5CG cap
+for res5pr in 5CG ; do
+
+mkdir $res5pr/
+cd $res5pr/
+
+echo "${ResNames[$res5pr]}" > $res5pr.modxna.in
+
+$modxna -i $res5pr.modxna.in -m $res5pr --5cap
+
+cd $WORKDIR
+
+done
+
+#3' 3EC cap
+for res3pr in 3EC ; do
+
+mkdir $res3pr/
+cd $res3pr/
+
+echo "${ResNames[$res3pr]}" > $res3pr.modxna.in
+
+$modxna -i $res3pr.modxna.in -m $res3pr --3cap
+
+cd $WORKDIR
+
+done
